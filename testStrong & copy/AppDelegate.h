09045f8520aa0bd44139f64a486f10eb69176508
huangshaohua <huangshaohua@xywy.com>
2015-12-22 19:30:38 +0800

    Initial Commit

---
+++ testStrong & copy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testStrong & copy
+//
+//  Created by 黄少华 on 15/12/22.
+//  Copyright © 2015年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testStrong & copy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testStrong & copy
+//
+//  Created by 黄少华 on 15/12/22.
+//  Copyright © 2015年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
